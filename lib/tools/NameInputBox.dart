9ed0ec22f7061fc59ea160b812661aae89a8aed1
HAHA <jperez5385@gmail.com>
2021-01-02 21:09:30 -0800

    input implemented

--- lib/tools/NameInputBox.dart
+++ lib/tools/NameInputBox.dart
@@ -25,16 +25,16 @@ class NameInputBox extends StatefulWidget {
 }
 
 class _NameInputBoxState extends State<NameInputBox> {
-  final List<Widget> children = <Widget>[];
-  double containerWidth;
+  double _containerWidth;
+  TextEditingController _textinputController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
       color: Colors.indigo,
       duration: Duration(milliseconds: 200),
-      width: containerWidth ?? widget.defaultWidth,
-      child: showDisplay(context),
+      width: _containerWidth ?? widget.defaultWidth,
+      child: showInputPrompt(context),
       onEnd: () {
         _collapseDisplay(false);
       },
@@ -44,13 +44,46 @@ class _NameInputBoxState extends State<NameInputBox> {
   _collapseDisplay(bool collapse) {
     setState(() {
       if (collapse) {
-        containerWidth = 0;
+        _containerWidth = 0;
       } else
-        containerWidth = widget.defaultWidth;
+        _containerWidth = widget.defaultWidth;
     });
   }
 
+
+  /// METHOD: SHOW INPUT PROMPT
+  /// ----------------------------------------
+  /// Get user input data
+  /// ----------------------------------------
+  Widget showInputPrompt(BuildContext context) {
+    String defaultText;
+    if(widget.title is Text) {
+      defaultText = (widget.title as Text).data;
+    }
+
+    return Container(
+      color: Colors.black,
+          child: TextFormField(
+        controller: _textinputController,
+        keyboardType: TextInputType.name,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          labelText: defaultText ?? 'Enter Text',
+          labelStyle: TextStyle(
+            color: Colors.white,
+          ),
+          hintStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// METHOD: SHOW_DISPLAY
+  /// --------------------------------------
   /// Builds the display mode of this widget
+  /// --------------------------------------
   Widget showDisplay(BuildContext context) {
     return Material(
       color: Colors.black,
@@ -72,7 +105,7 @@ class _NameInputBoxState extends State<NameInputBox> {
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
           child: DefaultTextStyle(
             child: widget.title,
-            overflow: TextOverflow.ellipsis,
+            overflow: TextOverflow.fade,
             softWrap: false,
             style: Theme.of(context)
                 .textTheme
